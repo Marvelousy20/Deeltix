96a752e8d9982956fd8194546f98d0e4e1065952
Marvelousy20 <afosmarvel@gmail.com>
2024-03-31 22:17:42 +0100

    wip

--- src/components/Dashboard/Reservation/upcoming-dropdown.tsx
+++ src/components/Dashboard/Reservation/upcoming-dropdown.tsx
@@ -6,6 +6,7 @@ import { ReminderNotification } from "./reminder-modal";
 import { useDisclosure } from "@mantine/hooks";
 import { Mail } from "lucide-react";
 import { ModalEvent } from "../GuestBook/event";
+import Modal from "./modal";
 
 export const UpcomingDropDown = ({
   reservationId,
@@ -29,37 +30,46 @@ export const UpcomingDropDown = ({
   const { push } = useRouter();
   const [opened, { open, close }] = useDisclosure();
   return (
-    <section className=" p-[15px] flex flex-col">
-      <div className="flex cursor-pointer items-center gap-3 border-b border-[#E7EAF1] pb-[12px]">
-        <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
-          <SearchStatus1 size={20} />
+    <div>
+      <section className="p-[15px] flex flex-col w-full">
+        <div className="flex cursor-pointer items-center gap-3 border-b border-[#E7EAF1] pb-[12px]">
+          <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
+            <SearchStatus1 size={20} />
+          </div>
+          <p className="text-base font-medium text-[#2C2929]">View</p>
         </div>
-        <p className="text-base font-medium text-[#2C2929]">View</p>
-      </div>
-      <div className="flex cursor-pointer items-center gap-3 border-b border-[#E7EAF1] py-[12px]">
-        <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
-          <Edit2 size={20} />
+        <div className="flex cursor-pointer items-center gap-3 border-b border-[#E7EAF1] py-[12px]">
+          <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
+            <Edit2 size={20} />
+          </div>
+          <p className="text-base font-medium text-[#2C2929]">Edit</p>
         </div>
-        <p className="text-base font-medium text-[#2C2929]">Edit</p>
-      </div>
-      <div className="flex cursor-pointer items-center gap-3 pt-[12px]">
-        <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
-          <Mail size={20} onClick={open} />
+        <div className="flex cursor-pointer items-center gap-3 pt-[12px]">
+          <div className="h-[30px] w-[30px] flex items-center justify-center rounded-full border border-[#E7EAF1]">
+            <Mail size={20} onClick={open} />
+          </div>
+          <p className="text-base font-medium text-[#2C2929]">Send reminder</p>
         </div>
-        <p className="text-base font-medium text-[#2C2929]">Send reminder</p>
+      </section>
+
+      <div>
+        {/* <ReminderNotification
+          opened={opened}
+          close={close}
+          user={user}
+          email={email}
+          number={number}
+          date={date}
+          time={time}
+          guest={guest}
+          request={request}
+          reservationId={reservationId}
+        /> */}
+
+        <>
+          <Modal onClose={close} opened={opened} />
+        </>
       </div>
-      <ReminderNotification
-        opened={opened}
-        close={close}
-        user={user}
-        email={email}
-        number={number}
-        date={date}
-        time={time}
-        guest={guest}
-        request={request}
-        reservationId={reservationId}
-      />
-    </section>
+    </div>
   );
 };
