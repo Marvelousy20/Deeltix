b927a65250dc93e2b2e223deb97c8cc24b1d049a
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-04-12 23:36:28 +0100

    fixed address nowrap

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -66,7 +66,7 @@ export default function CarouselSlider({ data }: RestaurantDetails) {
                   <div className="flex flex-col gap-2">
                     <div className="flex items-center gap-1">
                       <MapPin size={14} />
-                      <p className="text-sm whitespace-nowrap">{d.address}</p>
+                      <p className="text-sm">{d.address}</p>
                     </div>
 
                     <div className="flex items-center gap-2">
