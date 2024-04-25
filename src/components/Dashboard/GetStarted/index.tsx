4b8edaf1bb4f1ee88d29f14cd474c00fff3441b2
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-04-25 01:11:54 +0100

    handle state of sections in get started

--- src/components/Dashboard/GetStarted/index.tsx
+++ src/components/Dashboard/GetStarted/index.tsx
@@ -1,9 +1,29 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Headings } from "./Headings";
 import { Cards } from "./Card";
+import { ErrorType, handleError } from "@/lib/handle-error";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "@/axios-config";
 
 export const GettingStarted = () => {
+  const [sectionComp, SetSectionComp] = useState({})
+  const { mutate: checkIsComplete, isLoading: loadisComplete } = useMutation({
+    mutationFn: async (data: any) =>
+      await api.get(`/api/restaurant-manager/show-get-started`, data),
+    mutationKey: ["isComplete"],
+    onSuccess({data}) {
+      SetSectionComp(data.data.data);
+      console.log(data.data.data)
+    },
+    onError(error) {
+      handleError(error as ErrorType);
+    },
+  });
+
+  useEffect(() => {
+    checkIsComplete({})
+  }, [])
   return (
     <div className="lg:p-8 flex flex-col gap-8">
       <Headings
@@ -13,7 +33,7 @@ export const GettingStarted = () => {
         }
       />
       <div className="px-4 lg:px-0">
-        <Cards />
+        <Cards sectionComp={sectionComp}/>
       </div>
     </div>
   );
