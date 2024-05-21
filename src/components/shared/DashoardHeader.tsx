954431418d805140adc53f9522a547a630cd604a
Marvelousy20 <afosmarvel@gmail.com>
2024-05-22 00:42:12 +0100

    wip

--- src/components/shared/DashoardHeader.tsx
+++ src/components/shared/DashoardHeader.tsx
@@ -11,8 +11,24 @@ import { NotificationDrawer } from "./drawer";
 import { useDisclosure } from "@mantine/hooks";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/axios-config";
+import { usePathname } from "next/navigation";
 import { Turn as Hamburger } from "hamburger-react";
 import Image from "next/image";
+import { Isidebar } from "@/types";
+import { QrCode } from "lucide-react";
+import { clsx } from "@mantine/core";
+import {
+  Category,
+  Home2,
+  LogoutCurve,
+  People,
+  Profile,
+  Receipt,
+  Reserve,
+  Wallet,
+  Wallet3,
+} from "iconsax-react";
+import Link from "next/link";
 
 export const Header = () => {
   const [isOpen, setOpen] = useState(false);
@@ -23,7 +39,42 @@ export const Header = () => {
     fetchUser();
   }, []);
 
+  const sideBar: Isidebar[] = [
+    { name: "Get started", link: "/get-started", icon: <Category size="18" /> },
+
+    { name: "Dashboard", link: "/dashboard", icon: <Home2 size="18" /> },
+    { name: "Reservation", link: "/reservation", icon: <Receipt size={18} /> },
+    { name: "Menu", link: "/menu", icon: <Reserve size="18" /> },
+    {
+      name: "QR Code",
+      link: "/restaurant-qrcode",
+      icon: <QrCode size={18} />,
+    },
+    { name: "Guest Book", link: "/guest-book", icon: <People size="18" /> },
+    // {
+    //   name: "Wallet",
+    //   link: "/wallet",
+    //   icon: <Wallet3 size="18" />,
+    // },
+    {
+      name: "Profile",
+      link: "/restaurant-profile",
+      icon: <Profile size="18" />,
+    },
+    {
+      name: "Log out",
+      link: "/restaurant-dashboard",
+      icon: <LogoutCurve size="18" />,
+    },
+  ];
+
   const [opened, { open, close }] = useDisclosure();
+  const pathName = usePathname();
+  const { push } = useRouter();
+  const handleLogout = () => {
+    cookieStorage.clear();
+    push("/");
+  };
 
   return (
     <section className="flex lg:flex-row sticky top-0 z-[99] items-center gap-y-4 w-full justify-between py-4 px-4 md:px-8 backdrop-blur-md bg-[#F5F5F5]/50 border-b-[2px] border-grayBottom ">
@@ -80,7 +131,35 @@ export const Header = () => {
       {/* Mobile Navs */}
       {isOpen && (
         <div className="lg:hidden absolute top-full bg-[#111323] text-white w-full left-0 p-8">
-          this is the page
+          <div className="h-[30px] w-[30px] border rounded-full border-grayHeader bg-grayHeader flex items-center justify-center ">
+            <User size={20} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="text-[#FEFEFE] text-lg font-medium">
+              {restuarantName}
+            </h3>
+            <p className="font-normal text-lg text-grayInactive">
+              {managerName}
+            </p>
+          </div>
+
+          {sideBar.map(({ name, link, icon }, idx) => (
+            <Link
+              onClick={idx === 7 ? handleLogout : undefined}
+              href={link}
+              key={idx}
+              className={clsx(
+                link === pathName
+                  ? "text-red-500 outline-none border-[#636C71] rounded-[20px] p-4 !bg-blue-500 text-base font-medium"
+                  : "text-base font-medium text-white border-none"
+              )}
+            >
+              <div className="flex items-center gap-4 py-[8px] px-[10px]">
+                <span>{icon}</span>
+                <span>{name}</span>
+              </div>
+            </Link>
+          ))}
         </div>
       )}
     </section>
