d519636908b4e582d9f0886e5aa010444d88bd70
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-22 13:42:47 +0100

    grcode and menu page

--- src/app/restaurant/[id]/menu/page.tsx
+++ src/app/restaurant/[id]/menu/page.tsx
@@ -3,11 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { Receipt } from 'iconsax-react';
 import Image from 'next/image';
 import Accordion from './Accordion';
-import { MenuType } from '@/types';
+import {
+  MenuCategoryDetails,
+  MenuDetails,
+  MenuType,
+  UserSingleRestaurant,
+} from '@/types';
 import { useParams } from 'next/navigation';
 import { toast } from 'react-toastify';
-import { api } from '@/axios-config';
-import { useQuery } from '@tanstack/react-query';
+import { api, auth } from '@/axios-config';
+import { useQueries, useQuery } from '@tanstack/react-query';
 import Categories from './Categories';
 
 export interface IMenu {
@@ -23,54 +28,38 @@ type categoryType = {
 
 export default function Menu() {
   const { id } = useParams();
-  const [menu, setMenu] = useState<MenuType[]>([]);
-  const [categories, setCategories] = useState<categoryType[] | null>(null);
-
-  const fetchCategory = async () => {
-    if (!id) {
-      return;
-    }
-    try {
-      const result = await api.get(
-        `/api/restaurants/${id}/menu/categories`
-        // /api/restaurants/65ba1e5797aa8ffdb9208993/menu/categories'
-      );
-      const allCategories = result.data.data.data;
-      setCategories(allCategories.menuCategories);
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const { data: allCategories } = useQuery({
-    queryFn: fetchCategory,
-    queryKey: ['category restaurant'],
-  });
-
-  const { data, isLoading, isError, isSuccess } = useQuery({
-    queryKey: ['menu'],
-    queryFn: async () => {
-      if (!id) {
-        return;
-      }
-      return api.get(`/api/restaurants/${id}/menu`);
-    },
+  const { data, isLoading } = useQuery({
+    queryFn: async () =>
+      await auth.get<UserSingleRestaurant>(`/api/restaurants/${id}`),
+    queryKey: ['sigle-restaurat'],
     enabled: !!id,
+    select: ({ data }) => data?.data?.data?.restaurant,
   });
 
-  if (isError) {
-    toast.error('Something happened getting menu');
-  }
+  //Requuest to get restaurat category list
 
-  useEffect(() => {
-    fetchCategory();
-    if (isSuccess) {
-      setMenu(data?.data.data.data.menu);
-      console.log(data?.data.data.data.menu);
-    }
-    console.log(categories);
-  }, [id, data]);
+  const { data: category, isLoading: categoryLoading } = useQuery({
+    queryFn: async () =>
+      await auth.get<MenuCategoryDetails>(
+        `/api/restaurants/${id}/menu/categories`
+      ),
+    queryKey: ['menu-category'],
+  });
+
+  // Request to get each category menu
+  const categoryIds = category?.data?.data?.data?.menuCategories?.map((cat) => cat.id) || [];
+  const categoryQueries = useQueries({
+    queries: categoryIds.map((catId) => {
+      return {
+        queryKey: ['cat-menu', catId],
+        queryFn: async () => await auth.get(`/api/restaurants/${id}/menu?category=${catId}`),
+        // enabled: !!catId,
+      };
+    }),
+  });
+
+  console.log(categoryQueries)
+
   return (
     <div className="flex flex-col gap-4">
       <div className="text-white flex flex-col gap-4 bg-[url('/bg.svg')] bg-no-repeat bg-cover opacity-90 h-[40vh] items-center justify-center">
@@ -78,11 +67,9 @@ export default function Menu() {
             <Image src="" />
         </div> */}
         <div className="flex flex-col gap-3">
-          <p className="font-bold text-4xl text-center">
-            Cilantro Bar & Cusines
-          </p>
+          <p className="font-bold text-4xl text-center">{data?.name}</p>
           <p className="font-base font-medium text-center">
-            Sea side , family, lovers best restaurant
+            {data?.description}
           </p>
           <div
             className="flex px-[24px] py-[i6px] gap-6 bg-[#background: rgba(87, 77, 255, 0.2);
@@ -90,55 +77,84 @@ export default function Menu() {
 ]"
           >
             <div className="">
-              <span>i</span> 9am - 11am{' '}
+              <span>i</span> {data?.openingHour}-{data?.closingHour}
             </div>
             <div>
-              <span>i</span> 32 Lekki - Epe Expressway, Lekki peninsula 106104,
-              Lekki
+              <span>i</span> {data?.address}
             </div>
           </div>
-        </div>
-        <div className="mt-6 bg-[#574DFF] flex gap-2 rounded-3xl py-3 px-4">
-          <Receipt size={18} />
-          Make a reservation
+          <div className="mt-6 bg-[#574DFF] flex gap-2 rounded-3xl py-3 px-4">
+            <Receipt size={18} />
+            Make a reservation
+          </div>
         </div>
       </div>
-
       <div className="container flex flex-col">
         <div className="flex flex-row gap-4 items-center justify-center flex-wrap">
-          {categories?.map((cat, idx) => (
+          {category?.data?.data?.data?.menuCategories.map((cat, idx) => (
             <div key={idx}>
-
-                <Categories name={cat.name} />
+              <Categories name={cat.name} />
+              {/* <Categories name={cat.} /> */}
             </div>
           ))}
         </div>
 
-        <div className="w-[546px] ">
-        {menu?.map((aMenu, idx) => (
-          <Accordion key={idx} title={aMenu.name}>
-            <div className="flex bg-background rounded-lg w-[546px] h-fit">
-              <div className="p-3">
-                <Image src={`${aMenu.image}`} alt="menu-image" width={216} height={126} />
-              </div>
-              <div className="p-3 flex flex-col justify-between">
-                <div className="flex flex-col justify-between flex-wrap">
-                  <p className="text-lg font-semibold">
-                    {aMenu.name}
-                  </p>
-                  <p className="font-medium text-sm text-[#475467]">
-                    {aMenu.description}
+        {/* <div className="w-[546px]">
+          {categoryQueries.map((query, idx) => (
+            query.isSuccess && query.data?.data?.data?.data.menu.map((menu) => (
+              <Accordion key={menu.id} title={menu.name}>
+                <div className="flex bg-background rounded-lg w-[546px] h-fit">
+                  <div className="p-3">
+                    <Image
+                      src={menu.image}
+                      alt="menu-image"
+                      width={216}
+                      height={126}
+                    />
+                  </div>
+                  <div className="p-3 flex flex-col justify-between">
+                    <div className="flex flex-col justify-between flex-wrap">
+                      <p className="text-lg font-semibold">{menu.name}</p>
+                      <p className="font-medium text-sm text-[#475467]">
+                        {menu.description}
+                      </p>
+                    </div>
+                    <p className="text-primary font-semibold text-base">
+                      # {menu.price}
+                    </p>
+                  </div>
+                </div>
+              </Accordion>
+            ))
+          ))}
+        </div> */}
+        {/* <div className="w-[546px] ">
+          {menu?.data.map((aMenu, idx) => (
+            <Accordion key={idx} title={aMenu.name}>
+              <div className="flex bg-background rounded-lg w-[546px] h-fit">
+                <div className="p-3">
+                  <Image
+                    src={`${aMenu.image}`}
+                    alt="menu-image"
+                    width={216}
+                    height={126}
+                  />
+                </div>
+                <div className="p-3 flex flex-col justify-between">
+                  <div className="flex flex-col justify-between flex-wrap">
+                    <p className="text-lg font-semibold">{aMenu.name}</p>
+                    <p className="font-medium text-sm text-[#475467]">
+                      {aMenu.description}
+                    </p>
+                  </div>
+                  <p className="text-primary font-semibold text-base">
+                    # {aMenu.price}
                   </p>
                 </div>
-                <p className="text-primary font-semibold text-base">
-                  # {aMenu.price}
-                </p>
               </div>
-            </div>
-          </Accordion>
-
-        ))}
-        </div>
+            </Accordion>
+          ))}
+        </div> */}
       </div>
     </div>
   );
