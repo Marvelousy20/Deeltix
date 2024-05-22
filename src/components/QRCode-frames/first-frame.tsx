748631007fed23ec227c86f8a4e72d580b411615
alalade191 <alalade191@gmail.com>
2024-05-21 20:41:58 -0700

    Qr code update

--- src/components/QRCode-frames/first-frame.tsx
+++ src/components/QRCode-frames/first-frame.tsx
@@ -4,21 +4,26 @@ import { QRCode } from "react-qrcode-logo";
 export const FirstFrame = ({
   color,
   fcolor,
+  name,
+  numbers,
 }: {
   color: string;
   fcolor: string;
+  name: string;
+  numbers: any;
 }) => {
   return (
-    <div className="bg-black p-[4px] rounded-t-[8px] rounded-b-[20px] overflow-hidden">
+    <div className="bg-black p-[4px] rounded-t-[8px] rounded-b-[8px] overflow-hidden">
       <QRCode
         value="https://www.deeltix.com/"
         bgColor={`${color}`}
         fgColor={`${fcolor}`}
+        eyeRadius={numbers}
         // qrStyle={'fluid'}
         // logoImage="/dashboard/logo.svg"
       />
-      <div className="text-white flex items-center text-base justify-center">
-        Deeltix.com
+      <div className="text-white flex max-w-[150px] items-center text-base justify-center">
+        {name}
       </div>
     </div>
   );
--- src/components/QRCode-frames/second-frame.tsx
+++ src/components/QRCode-frames/second-frame.tsx
@@ -4,19 +4,24 @@ import { QRCode } from "react-qrcode-logo";
 export const SecondFrame = ({
   color,
   fcolor,
+  name,
+  numbers,
 }: {
   color: string;
   fcolor: string;
+  name: string;
+  numbers: any;
 }) => {
   return (
-    <div className="bg-black p-[4px] rounded-t-[20px] rounded-b-[8px]">
-      <div className="text-white text-base flex items-center justify-center">
-        Deeltix.com
+    <div className="bg-black p-[4px] rounded-b-[8px]">
+      <div className="text-white text-base max-w-[150px] flex items-center justify-center">
+        {name}
       </div>
       <QRCode
         value="https://www.deeltix.com/"
         bgColor={`${color}`}
         fgColor={`${fcolor}`}
+        eyeRadius={numbers}
         // qrStyle={'fluid'}
         // logoImage="/dashboard/logo.svg"
       />
